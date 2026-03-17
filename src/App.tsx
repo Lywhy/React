b5516dc6acfab5853fbe89555daed0a7bb308c53
Lywhy <123964598+Lywhy@users.noreply.github.com>
2026-03-17 20:40:28 +0300

    add ts

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState, MouseEvent } from "react"
+import Button from "./components/Button/Button"
+import Input from "./components/Input/Input"
+
+function App() {
+  const [counter, setCounter] = useState<number>(0)
+
+  const addCounter = (e: MouseEvent) => {
+    setCounter(prev => prev + 1)
+    console.log(counter, e)
+  }
+
+  return (
+    <>
+      <Button onClick={addCounter}>Кнопка</Button>
+      <Input placeholder="Email" />
+    </>
+  )
+}
+
+export default App
